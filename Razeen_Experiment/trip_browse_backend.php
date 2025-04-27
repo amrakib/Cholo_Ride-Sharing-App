27c2492fd3fa8f045dc9e3cb248b91d3faccf99a
RayzerGT <razeen.hassan@g.bracu.ac.bd>
2025-04-28 04:46:03 +0600

    Join trip page logic

--- Razeen_Experiment/trip_browse_backend.php
+++ Razeen_Experiment/trip_browse_backend.php
@@ -1,4 +1,15 @@
 <?php
 include '../frontend/connection.php';
-$location_value_fetch = "SELECT area FROM area_locations";
+$location_value_fetch = "SELECT * FROM Locations";
+
+$all_locs = $conn->query($location_value_fetch);
+
+if ($all_locs->num_rows > 0) {
+    while ($row = $all_locs->fetch_assoc()) {
+        $temp= "<option value=\"".$row["area_locations"]."\"".">".$row["area_locations"]."</option>";
+        echo $temp;
+}       
+} else {
+    echo "error";
+}
 ?>
